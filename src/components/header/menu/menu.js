1697c8719b29c3959c3eddc048df42f923d0501e
Pavel <lony2396@gmail.com>
2021-03-27 12:23:00 +0600

    getData

--- src/components/header/menu/menu.js
+++ src/components/header/menu/menu.js
@@ -3,7 +3,26 @@ import React, {Component} from 'react';
 
 
 export default class Menu extends Component {
+
+    getData = () => {
+        fetch('https://kinopoiskapiunofficial.tech/api/v2.1/films/1309596', {
+            headers:{
+                'X-API-KEY': '312e0cda-065d-48f5-9ff8-12be8a5e44e4',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            });
+    }
+
     render () {
+
+        this.getData();
+
         return (
             <div>
             <button  className="btn">Новости</button>
